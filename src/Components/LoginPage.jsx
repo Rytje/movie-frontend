919f58d200dac60bc6ea6301614dc2ed91c83691
Ryno <r_romario@msn.com>
2021-09-09 16:42:23 +0200

    Some progress on login-page

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Col, Container, Image, Row } from 'react-bootstrap';
 import logo from '../Assets/Netflix_Logo_RGB.png';
 
 export default function LoginPage() {
@@ -11,35 +12,41 @@ export default function LoginPage() {
     }
 
     return (
-        <div id="login-page">
-            <header>
-                <nav>
-                    <img id="logo" src={logo} alt="" />
-                </nav>
-            </header>
+        <Container fluid id="login-page">
+            <Row>
+                <Col xs={5}>
+                <header>
+                    <nav>
+                        {/* <img id="logo" src={logo} alt="" /> */}
+                        <Image fluid id="logo" src={logo} alt="Netflix logo" />
+                    </nav>
+                </header>
+                </Col>
 
-            <h1>Inloggen</h1>
+                <h1>Inloggen</h1>
 
-            <form action="" method="post">
-                <input type="email" name="email" id="email" placeholder="E-mailadres of telefoonnummer" />
-                <input type="password" name="password" id="passwordInput" placeholder="Wachtwoord" />
-                <button type="submit">Inloggen</button>
-                <label>
-                    Mijn gegevens onthouden
-                    <input type="checkbox" name="rememberCheck" id="rememberCheck" value={checkbox} onChange={handleChange} />
-                </label>
-            </form>
-            <a href="#">Hulp nodig?</a>
-            <div>
-                <img src="" alt="" />
-                <a href="#">Inloggen met Facebook</a>
-            </div>
-            <div>
-                <p>
-                    Is Netflix nieuw voor jou? <a href="#">Registreet je nu</a>.
-                </p>
-            </div>
-            <p>This page is protected by Google reCAPTCHA to ensure you're not a bot. <a href="#">Lees meer informatie</a>.</p>
-        </div>
+                <form action="" method="post">
+                    <input type="email" name="email" id="email" placeholder="E-mailadres of telefoonnummer" />
+                    <input type="password" name="password" id="passwordInput" placeholder="Wachtwoord" />
+                    <button type="submit">Inloggen</button>
+                    <label>
+                        Mijn gegevens onthouden
+                        <input type="checkbox" name="rememberCheck" id="rememberCheck" value={checkbox} onChange={handleChange} />
+                    </label>
+                </form>
+                <a href="#">Hulp nodig?</a>
+                <div>
+                    <img src="" alt="" />
+                    <a href="#">Inloggen met Facebook</a>
+                </div>
+                <div>
+                    <p>
+                        Is Netflix nieuw voor jou? <a href="#">Registreet je nu</a>.
+                    </p>
+                </div>
+                <p>This page is protected by Google reCAPTCHA to ensure you're not a bot. <a href="#">Lees meer informatie</a>.</p>
+            </Row>
+        </Container>
+
     )
 }
